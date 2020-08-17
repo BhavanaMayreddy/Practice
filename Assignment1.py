debfc63777dc364e593fb67c8a3d2fbacf248cb7
BhavanaMayreddy <60138520+BhavanaMayreddy@users.noreply.github.com>
2020-08-17 16:42:05 +0900

    Add files via upload
    Python practice with UI development using Django framework

---
+++ Assignment1.py
@@ -0,0 +1,29 @@
+# def is_leap():
+#     leap = False
+#
+#     # Write your logic here
+#     for year in range(1900,pow(10,5)):
+#         if 1900<=year<=pow(10,5):
+#             if year%4==0:
+#                 if year%100 !=0:
+#                     leap=True
+#                 elif year%400 == 0:
+#                     leap=True;
+#                 if leap == True:
+#                     print(year)
+#             # return leap
+#
+# # year = int(input())
+# print(is_leap())
+
+# def addition(a,b):
+#     sum = a+b
+#     return sum
+#
+# a=int(input())
+# b=int(input())
+# print(addition(2,3))
+
+if __name__ == '__main__':
+    n = int(input())
+    print(range(n))
